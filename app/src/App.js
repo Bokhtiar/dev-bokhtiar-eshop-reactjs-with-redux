d1c3819bd65ce647268615408a53e75fa00c654d
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-06-06 17:22:17 +0600

    admin dashboard templeting

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,40 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from "../src/Components/Admin/Layouts/Navbar"
+import Sidebar from "../src/Components/Admin/Layouts/Sidebar"
+import Home from "../src/Components/Admin/Home"
+
+
+
+// admin dashboard css file links
+import './assets/admin/assets/vendor/bootstrap/css/bootstrap.min.css'
+import './assets/admin/assets/vendor/bootstrap-icons/bootstrap-icons.css'
+import './assets/admin/assets/vendor/boxicons/css/boxicons.min.css'
+import './assets/admin/assets/vendor/quill/quill.snow.css'
+import './assets/admin/assets/vendor/quill/quill.bubble.css'
+import './assets/admin/assets/vendor/remixicon/remixicon.css'
+import './assets/admin/assets/vendor/simple-datatables/style.css'
+import './assets/admin/assets/css/style.css'
+
+
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Sidebar></Sidebar>
+
+        <main id="main" class="main">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </main>
+        
+      </BrowserRouter>
     </div>
   );
 }
---
+++ app/src/Components/Admin/Layouts/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer id="footer" className="footer">
+    <div className="copyright">
+      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+    </div>
+    <div className="credits">
+    </div>
+  </footer>
+  )
+}
+export default Footer
